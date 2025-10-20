185b898eddedce708d77c0bbdd7804dbec97f716
Samu <samukahonen@gmail.com>
2025-10-20 12:36:41 +0300

    server code update

--- submarine/main.py
+++ submarine/main.py
@@ -1,17 +1,25 @@
 import socket
 
-HOST = '192.168.1.10'  
-PORT = 5000         
+HOST = '169.254.1.1'  # Server's Ethernet IP
+PORT = 5001
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print(f"Connected to server {HOST}:{PORT}")
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
     
-    while True:
-        # Send message to server
-        msg = input("Your message: ")
-        s.sendall(msg.encode())
-        
-        # Receive response from server
-        data = s.recv(1024)
-        print(f"Server says: {data.decode()}")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    print("Client disconnected")
+                    break
+                command = data.decode()
+                print(f"Client command: {command}")
+            except ConnectionResetError:
+                print("Client disconnected unexpectedly")
+                break
